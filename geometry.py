33e6f8115ca6e838fd76110b5fd3b12377534384
Vilvi <tradegyzx@gmail.com>
2018-10-06 09:03:25 +0200

    Volume
    Added volume to circle and fixed some styling

--- geometry.py
+++ geometry.py
@@ -7,6 +7,7 @@ class Circle():
     pi = math.pi
     circumferenceFormula = '2π * r'
     areaFormula = 'π * r**2'
+    volumeFormula = '(4/3) * π * r**3'
 
     def __init__(self, r=1):
 
@@ -17,33 +18,52 @@ class Circle():
         return f'''
 The circumference of a circle is calculated by {self.circumferenceFormula}
 The area of a circle is calculated by {self.areaFormula}
+The volume of a sphere is calculated by {self.volumeFormula}
                 '''
 
     def circumference(self):
 
         print(f'Calculating {self.circumferenceFormula}')
+
         return 2 * math.pi * self.r
 
     def area(self):
 
         print(f'Calculating {self.areaFormula}')
+
         return math.pi * self.r**2
+
+    def volume(self):
+
+        print(f'Calculating {self.volumeFormula}')
+
+        return (4/3) * math.pi * self.r**3
 
 def calculateCircle(x):
 
     if x == 'a':
 
-        radius = int(input('What is the radius? '))
+        radius = float(input('What is the radius? '))
         circle = Circle(radius)
         area = circle.area()
+
         return area
 
     elif x == 'c':
 
-        radius = int(input('What is the radius? '))
+        radius = float(input('What is the radius? '))
         circle = Circle(radius)
         circumference = circle.circumference()
+
         return circumference
+
+    elif x == 'v':
+
+        radius = float(input('What is the radius? '))
+        circle = Circle(radius)
+        volume = circle.volume()
+
+        return volume
 
 # circle = Circle(3)
 # print(circle)
@@ -58,7 +78,7 @@ while ongoing:
 
         circle = Circle()
         print(circle)
-        geo = input('Do you want the area or the circumference? a = area, c = circumference. (q to quit) ').lower()
+        geo = input('Do you want the area, the circumference or the volume? a = area, c = circumference v = volume. (q to quit) ').lower()
 
         if geo == 'a':
 
@@ -70,11 +90,15 @@ while ongoing:
             result = calculateCircle('c')
             print(result)
 
+        elif geo == 'v':
+
+            result = calculateCircle('v')
+            print(result)
+
         elif geo == 'q':
 
             ongoing = False
             break
-
 
 
     elif shape == 'q':
