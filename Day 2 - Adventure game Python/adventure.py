a79e98808d06c7e5e700f43a082634e5ec488b97
Vilvi <tradegyzx@gmail.com>
2018-10-07 00:07:42 +0200

    Walking
    Started on the walking function

--- Day 2 - Adventure game Python/adventure.py
+++ Day 2 - Adventure game Python/adventure.py
@@ -7,10 +7,10 @@ def randomizeRoom():
 
     return random.choice(rooms)
 
-
 def encounterChance():
 
     monster = random.choice(list(creatures.keys()))
+    encounter = random.randint(0, 1)
 
     for k, v in creatures.items():
 
@@ -18,14 +18,22 @@ def encounterChance():
 
             experienceGain = v
 
-    encounter = random.randint(0, 1)
-
     if encounter == 0:
 
-        result = 'You did not encounter any monsters.'
+        return = 'but, you did not encounter any monsters.'
 
     elif encounter == 1:
 
-        result = f'You encounted a {monster} and gained {experienceGain} experience points!'
+        return = f'and, you encounted a {monster} and gained {experienceGain} experience points!'
 
-    return result
+def walking():
+
+    direction = input('Which way do you want to move? u = Up, d = Down, l = Left, r = Right q = Quit. ').lower()
+    room = random.choice(rooms)
+
+    if direction == 'u' or\
+       direction == 'd' or\
+       direction == 'l' or\
+       direction == 'r':
+
+       return f'You walked to the {room} {encounterChance()}'
